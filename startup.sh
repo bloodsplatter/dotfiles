8056ba09a2434f96109803dbbf01b854e7a955fe
Jonas Van der Aa <ketsukonn@gmail.com>
2019-09-17 19:38:54 +0200

    updated dotfiles for new install

--- startup.sh
+++ startup.sh
@@ -2,5 +2,19 @@
 # set a random wallpaper
 feh --randomize --bg-fill ~/Images/Wallpapers/* &
 
-# update newsboat
-newsboat -x reload &
+# launch notification daemon
+twmnd &
+
+# composer
+compton --backend glx --vsync & >/dev/null
+
+# kill current polybars
+killall -q polybar
+
+#wait for kill to finish
+while pgrep -u $UID -x polybar >/dev/null; do
+	sleep 1; done
+
+
+polybar -r top & >/dev/null
+polybar -r bottom & >/dev/null
--- unread-news.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/env bash
-# currently this does not work if newsboat is open
-unread= newsboat -x print-unread | awk '{print $1}'
-echo "$unread"
